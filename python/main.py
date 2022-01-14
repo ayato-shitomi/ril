a3117c30bec8a09d5f4765d47acde604889cc32e
Your Name <you@example.com>
2022-01-14 12:48:33 +0900

    made playershtml

--- python/main.py
+++ python/main.py
@@ -13,11 +13,11 @@ for data in lisData:
 	print('	"playerText" : "' + data2[1] + '",')
 	print('	"TwitterLnk" : "' + data2[2] + '",')
 	if (data2[3] == "ダイヤ"):
-		print('	"MaxRanked" : "' + "d" + '",')
+		print('	"MaxRanked" : "' + "d" + '"')
 	elif (data2[3] == "マスター"):
-		print('	"MaxRanked" : "' + "m" + '",')
+		print('	"MaxRanked" : "' + "m" + '"')
 	elif (data2[3] == "プレデター"):
-		print('	"MaxRanked" : "' + "p" + '",')
+		print('	"MaxRanked" : "' + "p" + '"')
 	else:
-		print('	"MaxRanked" : "' + "n" + '",')
+		print('	"MaxRanked" : "' + "n" + '"')
 	print("},")
